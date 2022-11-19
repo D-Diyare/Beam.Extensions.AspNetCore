d1baadd8ba393523d2d50ee6a897e8e778e136a3
Diyare Adbulla <codexsolutions2020@gmail.com>
2022-11-19 13:29:47 +0300

    Making improvements in package, add sample project.

---
+++ Beam.Extensions.AspNetCore.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Beam.Extensions.AspNetCore.Sample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Beam.Extensions.AspNetCore
+
+A Set of ASP.NET Core 7 extensions to register dbcontext, identity, JWT, localization, compression...
